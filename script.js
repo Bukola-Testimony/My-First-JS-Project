31f17c1d92d3cfb89c7039266316efd7ddcc28bf
Bukola Testimony <102736653+Bukola-Testimony@users.noreply.github.com>
2022-06-29 00:10:11 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+function loginUser() {
+  let userName = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+  let confirmPassword = document.getElementById("confirm-password").value
+
+ if(userName == null || userName == "" ){
+    return
+  }
+  else if(userName.length <= 10 && password === confirmPassword){
+    alert(`Hello ${userName}, login successful!`)
+  }
+  else if (userName.length > 10 ){
+    alert("invalid input! Username must be less than 10 characters.")
+  }
+  else if (password !== confirmPassword ){
+    alert("invalid input! Password does not match! ")
+  }
+  
+  else{
+    alert("login failed!!")
+  }
+}
+
+
+
+
